a766d67859c463641fd823c2971d388599341482
Akhil Prabhu <prabhuakhil2@gmail.com>
2025-09-02 00:28:56 +0530

    Update page.tsx

---
+++ app/search/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const SearchPage = () => {
+	return (
+		<main className="flex flex-col items-center min-h-screen bg-gray-50 p-8">
+			<h1 className="text-2xl font-bold">Search Page</h1>
+			{/* Add your search UI here */}
+		</main>
+	);
+};
+
+export default SearchPage;
